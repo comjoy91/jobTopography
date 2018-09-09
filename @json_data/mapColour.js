1b72ce3e78ca287589f799a17f537c364e65f3bd
comjoy91 <comjoy91@gmail.com>
2018-09-09 20:18:16 +0900

    색상 블랜딩 추가: (1) background layer들은 tilePane에 넣고, (2) overlay layer들은 overlayPane에 넣었으며, (3) mix-blend-mode: multiply. 단 overlayPane에 color-burn 추가.

---
+++ @json_data/mapColour.js
@@ -0,0 +1,23 @@
+var _mapColour = {
+	"hiringRate_0": [
+		'#FFFFFF', '#FFF5EB', '#FEE6CE', '#FDD0A2', '#FDAE6B', '#FD8D3C', '#F16913', '#D94801', '#A63603', '#7F2704'
+	], 
+	"hiringRate_300": [
+		'#FFFFFF', '#FFF5EB', '#FEE6CE', '#FDD0A2', '#FDAE6B', '#FD8D3C', '#F16913', '#D94801', '#A63603', '#7F2704'		
+	], 
+	"hiringRate_1000": [
+		'#FFFFFF', '#FFF5EB', '#FEE6CE', '#FDD0A2', '#FDAE6B', '#FD8D3C', '#F16913', '#D94801', '#A63603', '#7F2704'		
+	], 
+	"extinctIndex": [
+		'#FFFFFF', '#F8E3FF', '#F0C7FF', '#E9ABFF', '#E28FFF', '#DB73FF', '#D357FF', '#CC3BFF', '#C51FFF', '#BD00FF'		
+	], 
+	"incomeRate": [
+		'#FFFFFF', '#FFF5EB', '#FEE6CE', '#FDD0A2', '#FDAE6B', '#FD8D3C', '#F16913', '#D94801', '#A63603', '#7F2704'		
+	], 
+	"R_COSTII": [
+		'#FFFFFF', '#FFF5EB', '#FEE6CE', '#FDD0A2', '#FDAE6B', '#FD8D3C', '#F16913', '#D94801', '#A63603', '#7F2704'		
+	], 
+	"expertRate": [
+		'#FFFFFF', '#FFF5EB', '#FEE6CE', '#FDD0A2', '#FDAE6B', '#FD8D3C', '#F16913', '#D94801', '#A63603', '#7F2704'		
+	]
+};
